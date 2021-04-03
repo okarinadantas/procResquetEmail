4a56432b2f0c9f39a524402b21520b95ca45f9a5
Karina Dantas Luiz <karinadantasluiz@gmail.com>
2021-04-03 20:46:48 -0300

    versao 1.2
    alteração na proc - validação email e senha

--- resquetEmail.sql
+++ resquetEmail.sql
@@ -7,28 +7,30 @@ begin
 drop procedure Retornalogin
 end
 
-go 
-
---SELECT *
---FROM   [Person].[emailaddress]
-
 go
 
-CREATE PROCEDURE Retornalogin  @email   VARCHAR (40)
+CREATE PROCEDURE Retornalogin  
+
+@email   VARCHAR (40),
+@senha varchar (10)
 AS
   BEGIN
       SET nocount ON
 
-      SELECT [emailaddress]
-      FROM   [Person].[emailaddress]
-      WHERE   @email = [emailaddress]
+      SELECT PE.[emailaddress], PP.[PasswordSalt]
+      FROM   [Person].[emailaddress] PE
+	  JOIN [Person].[Password] PP ON PP.[BusinessEntityID] = PE.[BusinessEntityID]
+      WHERE   @email = [emailaddress] AND @senha = [PasswordSalt]
 
       SET nocount OFF
   END;
 
   go
 
+--retorno teste
+--EXEC Retornalogin   'ken0@adventure-works.com', 'bE3XiWw=' 
 
---EXEC Retornalogin   'janice0@adventure-works.com' 
 
-
+ --SELECT top 5 PE.[emailaddress], PP.[PasswordSalt]
+      --FROM   [Person].[emailaddress] PE
+	  --JOIN [Person].[Password] PP ON PP.[BusinessEntityID] = PE.[BusinessEntityID]
